cee77ee36bfa49c75ca143255ed46d3d0e940b57
Daniel Te <dte.biotech46@gmail.com>
2026-04-25 01:59:28 -0400

    Added docs and FUWAMOCO hashtags

---
+++ README.md
@@ -0,0 +1,8 @@
+# VHash
+
+A lookup service for VTuber hashtags
+
+## Sources
+1. Hashtags: https://virtualyoutuber.fandom.com/wiki/Virtual_YouTuber_Wiki
+2. Python sqlite3 module: https://www.w3schools.com/python/ref_module_sqlite3.asp
+3. https://www.w3schools.com/sql/default.asp
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import sqlite3
 
 conn = sqlite3.connect(':memory:')
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE users (name TEXT, age INTEGER)')
-cursor.execute("INSERT INTO users VALUES ('Tobias', 28)")
+cursor.execute('CREATE TABLE vtubers (name TEXT, general TEXT, fan_art TEXT, suggestions TEXT, fuwamoco_morning TEXT, clips TEXT, music TEXT, memes TEXT)')
+cursor.execute("INSERT INTO vtubers VALUES ('FUWAMOCO', '#FUWAMOCO', '#FWMCpix', '#helpFWMC', '#FWMCMORNING', '#lilFWMC', '#FWMCbeats', '#FWMCwww')")
 conn.commit()
 
-cursor.execute('SELECT * FROM users')
+cursor.execute("SELECT * FROM vtubers WHERE name = 'FUWAMOCO'")
 result = cursor.fetchone()
-print(f'User: {result[0]}, Age: {result[1]}')
+print(f'Vtuber: {result[0]}\nGeneral Hashtag: {result[1]}\nFan Art Hashtag: {result[2]}\nSuggestions Hashtag: {result[3]}\nFUWAMOCO Morning Hashtag: {result[4]}\nClips Hashtag: {result[5]}\nMusic Hashtag: {result[6]}\nMemes Hashtag: {result[7]}')
