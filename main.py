38e38b8a58ad10b8a4da91018c40a4719f50d327
Daniel Te <dte.biotech46@gmail.com>
2026-04-28 20:16:29 -0400

    Added Lazuli

--- main.py
+++ main.py
@@ -1,11 +1,78 @@
 import sqlite3
 
+# establish connection to SQLite DB
+
 conn = sqlite3.connect(':memory:')
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE vtubers (name TEXT, general TEXT, fan_art TEXT, suggestions TEXT, fuwamoco_morning TEXT, clips TEXT, music TEXT, memes TEXT)')
-cursor.execute("INSERT INTO vtubers VALUES ('FUWAMOCO', '#FUWAMOCO', '#FWMCpix', '#helpFWMC', '#FWMCMORNING', '#lilFWMC', '#FWMCbeats', '#FWMCwww')")
+
+
+# defining the structure of a VTuber hashtag record
+
+cursor.execute('''
+    CREATE TABLE vtubers 
+               (name TEXT, 
+               general TEXT, 
+               fan_art TEXT, 
+               suggestions TEXT, 
+               morning_show TEXT, 
+               clips TEXT, 
+               music TEXT, 
+               memes TEXT,
+               live TEXT,
+               NSFW text)
+''')
+
+
+# insert various VTubers into the table
+
+cursor.execute('''
+               INSERT INTO vtubers VALUES 
+               ('FUWAMOCO', 
+               '#FUWAMOCO', 
+               '#FWMCpix', 
+               '#helpFWMC', 
+               '#FWMCMORNING', 
+               '#lilFWMC', 
+               '#FWMCbeats', 
+               '#FWMCwww',
+               'None',
+               'None')
+''')
+
+cursor.execute('''
+               INSERT INTO vtubers VALUES 
+               ('Lazuli', 
+               '#lazufish', 
+               '#paintfins', 
+               'None',
+               'None',
+               '#lazufishnclips',
+               'None',
+               '#lazulaff',
+               '#lazulazulook',
+               '#lazukashii')
+''')
+
 conn.commit()
 
-cursor.execute("SELECT * FROM vtubers WHERE name = 'FUWAMOCO'")
+
+# temporary
+
+cursor.execute("SELECT * FROM vtubers WHERE name = 'Lazuli'")
+
+
+# print the entire hashtag record
+
 result = cursor.fetchone()
-print(f'Vtuber: {result[0]}\nGeneral Hashtag: {result[1]}\nFan Art Hashtag: {result[2]}\nSuggestions Hashtag: {result[3]}\nFUWAMOCO Morning Hashtag: {result[4]}\nClips Hashtag: {result[5]}\nMusic Hashtag: {result[6]}\nMemes Hashtag: {result[7]}')
+print(f'''
+    Vtuber: {result[0]}\n
+    General Hashtag: {result[1]}\n
+    Fan Art Hashtag: {result[2]}\n
+    Suggestions Hashtag: {result[3]}\n
+    Morning show Hashtag: {result[4]}\n
+    Clips Hashtag: {result[5]}\n
+    Music Hashtag: {result[6]}\n
+    Memes Hashtag: {result[7]}\n
+    Livestream Hashtag: {result[8]}\n
+    NSFW Hashtag: {result[8]}\n
+''')
